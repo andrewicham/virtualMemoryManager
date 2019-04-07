687bc0a83273e7455e31c051d1f1bdbb0996055b
andrewicham <andrewicham@gmail.com>
2019-04-07 14:13:21 -0400

    created a function which calculates offset and page number using bit shifting and bit masking

--- virtualMemoryManager.c
+++ virtualMemoryManager.c
@@ -10,43 +10,49 @@
 #define TLB_SIZE 16
 #define PAGE_NUMBER_MASK 0xffff
 #define OFFSET_MASK 0xff
+#define VIRTUAL_ADDRESS_SIZE 10
 
-int pageTable[TOTAL_FRAMES][FRAME_SIZE];
-
+int physicalMemory[TOTAL_FRAMES][FRAME_SIZE];
+int pageTable[PAGE_TABLE_SIZE][PAGE_SIZE];
+char virtualAddress[VIRTUAL_ADDRESS_SIZE];
 void getPage(int virtualAddress);
 
-
+struct Page{
+    int virtualPageNumber;
+    int mappedFrame;
+    struct Page *next;
+};
 
 
 void getPage(int virtualAddress){
     int pageNumber = ((virtualAddress & PAGE_NUMBER_MASK) >> 8); //this will shift right 8 bits, after performing and on both
     int offset = (virtualAddress & OFFSET_MASK); //performs and on virtualAddress to determine offset
 
-    printf("page number: %d\noffset:%d\n", pageNumber, offset);
+    printf("page number: %d offset:%d\n", pageNumber, offset);
 }
 
 
 
 int main(int argc, char const *argv[]){
-    //as per the instructions, use bitmask and bit shifting to extract address and offset on this array
-    int nums[7] = {1, 256, 32768, 32769, 128, 65534, 33153}; 
-    int i;
-
-    for(i = 0; i < 7; i++){
-        getPage(nums[i]);
-    }
-    
-
     FILE *addresses; //creates a file pointer to addresses file
     FILE *backingStore;
 
     addresses = fopen(argv[1], "r"); //reads in file as command line argument 
     backingStore = fopen("BACKING_STORE.bin", "rb"); //opens the backing_store.bin file
-
-
-
     
 
+    int logicalAddress;
+    while(fgets(virtualAddress, VIRTUAL_ADDRESS_SIZE, addresses) != NULL){
+        logicalAddress = atoi(virtualAddress);
+
+        getPage(logicalAddress);
+    }
+
+    fclose(addresses);
+    fclose(backingStore);
+    
+
+    
 
     return 0;
 }
