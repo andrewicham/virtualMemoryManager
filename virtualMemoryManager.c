981ec1556bdd837b067ad4610ec9d05d321b1d0c
andrewicham <andrewicham@gmail.com>
2019-04-08 16:59:31 -0400

    the program now reads integer values from a file and translates them into page numbers and offsets, using bit-wise operators.

--- virtualMemoryManager.c
+++ virtualMemoryManager.c
@@ -2,35 +2,41 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdint.h>
 
 #define PAGE_TABLE_SIZE 256
 #define PAGE_SIZE 256
 #define TOTAL_FRAMES 256
 #define FRAME_SIZE 256
 #define TLB_SIZE 16
-#define PAGE_NUMBER_MASK 0xffff
+#define PAGE_NUMBER_MASK 0xff00 //could be either 0xffff or 0xff00, as we are losing right most two f's 
 #define OFFSET_MASK 0xff
 #define VIRTUAL_ADDRESS_SIZE 10
+#define OFFSET_LENGTH 8
 
-int physicalMemory[TOTAL_FRAMES][FRAME_SIZE];
-int pageTable[PAGE_TABLE_SIZE][PAGE_SIZE];
-char virtualAddress[VIRTUAL_ADDRESS_SIZE];
-void getPage(int virtualAddress);
+//using macros for simplicity instead of functions
+#define GET_PAGE_NUMBER(addr) (((addr) & (PAGE_NUMBER_MASK)) >> (OFFSET_LENGTH))
+#define GET_OFFSET(addr) ((addr) & (OFFSET_MASK))
+#define GET_PHYSICAL_ADDRESS(frame, offset) (((frame) << (OFFSET_LENGTH) | (offset)))
+
 
 struct Page{
-    int virtualPageNumber;
-    int mappedFrame;
-    struct Page *next;
+    int pageNumber;
+    int frameNumber;
 };
 
+struct TLB{
+    struct Page pages[TLB_SIZE];
+};
 
-void getPage(int virtualAddress){
-    int pageNumber = ((virtualAddress & PAGE_NUMBER_MASK) >> 8); //this will shift right 8 bits, after performing and on both
-    int offset = (virtualAddress & OFFSET_MASK); //performs and on virtualAddress to determine offset
+struct Frame {
+    uint8_t valid;
+    char bytes[FRAME_SIZE];
+};
 
-    printf("page number: %d offset:%d\n", pageNumber, offset);
-}
-
+struct Frame physicalMemory[TOTAL_FRAMES];
+struct Page pageTable[PAGE_TABLE_SIZE];
+char virtualAddress[VIRTUAL_ADDRESS_SIZE];
 
 
 int main(int argc, char const *argv[]){
@@ -44,15 +50,15 @@ int main(int argc, char const *argv[]){
     int logicalAddress;
     while(fgets(virtualAddress, VIRTUAL_ADDRESS_SIZE, addresses) != NULL){
         logicalAddress = atoi(virtualAddress);
+        uint32_t pageNumber = GET_PAGE_NUMBER(logicalAddress);
+        uint32_t offset = GET_OFFSET(logicalAddress);
 
-        getPage(logicalAddress);
+        printf("PageNumber: %u Offset: %u\n", pageNumber, offset);
+
     }
 
     fclose(addresses);
     fclose(backingStore);
-    
-
-    
 
     return 0;
 }
